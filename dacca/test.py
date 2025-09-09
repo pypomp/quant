f57a467cd8f6cbcf415a2b9dec78adac7659a7b6
Aaron Abkemeier <aaronabkemeier@gmail.com>
2025-09-09 11:32:40 -0400

    Update several files

--- dacca/test.py
+++ dacca/test.py
@@ -27,73 +27,43 @@ NP_EVAL = (2, 1000, 1000, 5000)[RUN_LEVEL - 1]
 NREPS_EVAL = (2, 5, 24, 36)[RUN_LEVEL - 1]
 print(f"Running at level {RUN_LEVEL}")
 
-RW_SD = jnp.array([0.02] * 21)
+# rho fixed at 0
+RW_SD = jnp.array([0.02] * 2 + [0.0] + [0.02] * 18)
 RW_SD_INIT = jnp.array([0.0] * 21)
 COOLING_RATE = 0.5
 
-
-# TODO: Handle initial parameter selection
-# params_box = {
-#     "R0": [10, 60],
-#     "rho": [0.1, 0.9],
-#     "sigmaSE": [0.04, 0.1],
-#     "amplitude": [0.1, 0.6],
-#     "S_0": [0.01, 0.07],
-#     "E_0": [0.000004, 0.0001],
-#     "I_0": [0.000003, 0.001],
-#     "R_0": [0.9, 0.99],
-#     "sigma": [25, 100],
-#     "iota": [0.004, 3],
-#     "psi": [0.05, 3],
-#     "cohort": [0.1, 0.7],
-#     "gamma": [25, 320],
-# }
-
-# key, subkey = jax.random.split(key)
-# initial_params_list = pp.Pomp.sample_params(params_box, NREPS_FITR, key=subkey)
-
-# transformed_params_list = []
-# for params in initial_params_list:
-#     transformed_params = {}
-#     for k, v in params.items():
-#         if k in ["R0", "sigmaSE", "sigma", "iota", "psi", "gamma"]:
-#             transformed_params[k] = float(np.log(v))
-#         elif k in ["rho", "amplitude", "cohort"]:
-#             transformed_params[k] = float(pp.logit(v))
-#         else:
-#             transformed_params[k] = v
-#     transformed_params_list.append(transformed_params)
-
-
 dacca_obj = pp.dacca()
 
-print("Starting IF2")
+params_box = {k: [v * 0.5, v * 1.5] for k, v in dacca_obj.theta[0].items()}
+params_box["rho"] = [0.0, 0.0]
 key, subkey = jax.random.split(key)
-start_time = time.time()
-dacca_obj.mif(
-    # theta=transformed_params_list,
-    sigmas=RW_SD,
-    sigmas_init=RW_SD_INIT,
-    M=NFITR,
-    a=COOLING_RATE,
-    J=NP_FITR,
-    key=subkey,
-)
-print(f"mif time taken: {time.time() - start_time} seconds")
+initial_params_list = pp.Pomp.sample_params(params_box, NREPS_FITR, key=subkey)
 
-start_time = time.time()
-dacca_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL, key=subkey)
-print(f"pfilter time taken: {time.time() - start_time} seconds")
+with jax.profiler.trace("dacca_profiler"):
+    key, subkey = jax.random.split(key)
+    dacca_obj.pfilter(theta=initial_params_list, J=NP_EVAL, reps=NREPS_EVAL, key=subkey)
+    dacca_obj.mif(
+        theta=initial_params_list,
+        sigmas=RW_SD,
+        sigmas_init=RW_SD_INIT,
+        M=NFITR,
+        a=COOLING_RATE,
+        J=NP_FITR,
+    )
+    dacca_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL)
+    # dacca_obj.train(J=NP_FITR, M=NTRAIN, eta=0.2)
+    # dacca_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL)
+    dacca_obj.prune(n=3)
+    dacca_obj.mif(
+        sigmas=RW_SD / 4,
+        sigmas_init=RW_SD_INIT / 4,
+        M=NFITR,
+        a=COOLING_RATE,
+        J=NP_FITR,
+    )
+    dacca_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL)
 
-# start_time = time.time()
-# dacca_obj.train(J=NP_FITR, reps=NTRAIN, key=subkey)
-# print(f"train time taken: {time.time() - start_time} seconds")
-
-# start_time = time.time()
-# dacca_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL, key=subkey)
-# print(f"pfilter time taken: {time.time() - start_time} seconds")
-
-print(dacca_obj.results())
+print(dacca_obj.time())
 
 with open("dacca_results.pkl", "wb") as f:
     pickle.dump(dacca_obj, f)
