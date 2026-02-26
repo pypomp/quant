44888ca4ad5731307ab8eb1be514ef64767adb74
Aaron Abkemeier <aaronabkemeier@gmail.com>
2026-02-26 12:54:03 -0500

    Minor edits to continuous measles model test

--- tests/measles/continuous/IFAD_test.py
+++ tests/measles/continuous/IFAD_test.py
@@ -1,8 +1,8 @@
 # --- SLURM CONFIG ---
 # sbatch_args:
 #   job-name: "pypomp continuous measles test (IFAD)"
-#   partition: gpu_mig40
-#   gpus: "nvidia_a100_80gb_pcie_3g.40gb:1"
+#   partition: gpu
+#   gpus: "v100:1"
 #   cpus-per-gpu: 1
 #   mem: 6GB
 #   output: "IFAD_results/logs/slurm-%j.out"
@@ -15,8 +15,12 @@
 #   3:
 #     sbatch_args: { time: "00:30:00" }
 #   4:
-#     sbatch_args: { time: "01:00:00" }
+#     sbatch_args: { time: "00:30:00" }
 # --- END SLURM CONFIG ---
+
+import os
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.0"
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
 import jax
 import pickle
@@ -28,8 +32,8 @@ from setup import (
     RUN_LEVEL,
 )
 
-NFITR = (2, 20, 100, 100)[RUN_LEVEL - 1]
-NTRAIN = (2, 20, 40, 40)[RUN_LEVEL - 1]
+NFITR = (2, 20, 100, 40)[RUN_LEVEL - 1]
+NTRAIN = (2, 20, 40, 60)[RUN_LEVEL - 1]
 NP_FITR = (2, 3, 20, 10000)[RUN_LEVEL - 1]
 NP_EVAL = (2, 20, 1000, 10000)[RUN_LEVEL - 1]
 NREPS_EVAL = (2, 5, 24, 36)[RUN_LEVEL - 1]
@@ -52,19 +56,19 @@ eta = {
 }
 
 key, subkey = jax.random.split(key)
-# measles_obj.mif(
-#     rw_sd=RW_SD,
-#     M=NFITR,
-#     a=COOLING_RATE,
-#     J=NP_FITR,
-#     key=subkey,
-# )
+measles_obj.mif(
+    rw_sd=RW_SD,
+    M=NFITR,
+    a=COOLING_RATE,
+    J=NP_FITR,
+    key=subkey,
+)
 measles_obj.train(
-    J=NP_FITR, M=NTRAIN, eta=eta, optimizer="Adam", n_monitors=1, key=subkey
+    J=NP_FITR, M=NTRAIN, eta=eta, optimizer="Adam", n_monitors=1
 )
 measles_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL)
-# measles_obj.prune(n=1, refill=False)
-# measles_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL, CLL=True)
+measles_obj.prune(n=1, refill=False)
+measles_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL, CLL=True)
 
 measles_obj.print_summary()
 print(measles_obj.time())
