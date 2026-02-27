06db83418ff75db40450d2eb859b16cf5346e619
Aaron Abkemeier <aaronabkemeier@gmail.com>
2026-02-27 13:01:34 -0500

    Add continuous panel measles tests

---
+++ tests/panel_measles/continuous/IFAD_test.py
@@ -0,0 +1,76 @@
+# --- SLURM CONFIG ---
+# sbatch_args:
+#   job-name: "pypomp panel continuous measles test (IFAD)"
+#   partition: gpu
+#   gpus: "v100:1"
+#   cpus-per-gpu: 1
+#   mem: 6GB
+#   output: "IFAD_results/logs/slurm-%j.out"
+#   account: "ionides0"
+# run_levels:
+#   1:
+#     sbatch_args: { time: "00:03:00" }
+#   2:
+#     sbatch_args: { time: "00:20:00" }
+#   3:
+#     sbatch_args: { time: "03:00:00" }
+#   4:
+#     sbatch_args: { time: "05:00:00" }
+# --- END SLURM CONFIG ---
+
+import os
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.0"
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+
+import jax
+import pickle
+from setup import (
+    key,
+    RW_SD,
+    COOLING_RATE,
+    panel_measles_obj,
+    RUN_LEVEL,
+)
+
+NFITR = (2, 20, 100, 40)[RUN_LEVEL - 1]
+NTRAIN = (2, 20, 40, 60)[RUN_LEVEL - 1]
+NP_FITR = (2, 3, 20, 10000)[RUN_LEVEL - 1]
+NP_EVAL = (2, 20, 1000, 10000)[RUN_LEVEL - 1]
+NREPS_EVAL = (2, 5, 24, 36)[RUN_LEVEL - 1]
+
+DEFAULT_ETA = 0.02
+eta = {
+    "R0": DEFAULT_ETA,
+    "sigma": DEFAULT_ETA,
+    "gamma": DEFAULT_ETA,
+    "iota": DEFAULT_ETA,
+    "rho": DEFAULT_ETA,
+    "sigmaSE": DEFAULT_ETA,
+    "psi": DEFAULT_ETA,
+    "cohort": DEFAULT_ETA,
+    "amplitude": DEFAULT_ETA,
+    "S_0": DEFAULT_ETA,
+    "E_0": DEFAULT_ETA,
+    "I_0": DEFAULT_ETA,
+    "R_0": DEFAULT_ETA,
+}
+
+key, subkey = jax.random.split(key)
+panel_measles_obj.mif(
+    rw_sd=RW_SD,
+    M=NFITR,
+    a=COOLING_RATE,
+    J=NP_FITR,
+    key=subkey,
+)
+
+panel_measles_obj.train(J=NP_FITR, M=NTRAIN, eta=eta, optimizer="Adam")
+panel_measles_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL)
+panel_measles_obj.prune(n=1)
+panel_measles_obj.pfilter(J=NP_EVAL, reps=NREPS_EVAL, CLL=True)
+
+panel_measles_obj.print_summary()
+print(panel_measles_obj.time())
+
+with open(f"IFAD_results/panel_measles_results_rl{RUN_LEVEL}.pkl", "wb") as f:
+    pickle.dump(panel_measles_obj, f)
---
+++ tests/panel_measles/continuous/setup.py
@@ -0,0 +1,118 @@
+"""
+This script sets up the continuous panel UK measles model.
+"""
+
+import os
+from datetime import datetime
+from importlib.metadata import version
+import jax
+import pypomp as pp
+import numpy as np
+
+print(jax.devices())
+
+now = datetime.now()
+print("DATE: ", now.date())
+print("TIME: ", now.time())
+print("pypomp version:", version("pypomp"))
+print("jax version:", version("jax"))
+
+MAIN_SEED = 631409
+key = jax.random.key(MAIN_SEED)
+np.random.seed(MAIN_SEED)
+
+RUN_LEVEL = int(os.environ.get("RUN_LEVEL", "1"))
+
+NREPS_FITR = (2, 3, 20, 36)[RUN_LEVEL - 1]
+
+print(f"Running at level {RUN_LEVEL}")
+
+N_UNITS = 5
+ALL_UNITS = [
+    "Bedwellty",
+    "Birmingham",
+    "Bradford",
+    "Bristol",
+    "Cardiff",
+    "Consett",
+    "Dalton.in.Furness",
+    "Halesworth",
+    "Hastings",
+    "Hull",
+    "Leeds",
+    "Lees",
+    "Liverpool",
+    "London",
+    "Manchester",
+    "Mold",
+    "Northwich",
+    "Nottingham",
+    "Oswestry",
+    "Sheffield",
+]
+UNITS = ALL_UNITS[:N_UNITS]
+
+DEFAULT_SD = 0.02
+DEFAULT_IVP_SD = DEFAULT_SD * 12
+RW_SD = pp.RWSigma(
+    sigmas={
+        "R0": DEFAULT_SD * 0.25,
+        "sigma": DEFAULT_SD * 0.25,
+        "gamma": DEFAULT_SD * 0.5,
+        "iota": DEFAULT_SD,
+        "rho": DEFAULT_SD * 0.5,
+        "sigmaSE": DEFAULT_SD,
+        "psi": DEFAULT_SD * 0.25,
+        "cohort": DEFAULT_SD * 0.5,
+        "amplitude": DEFAULT_SD * 0.5,
+        "S_0": DEFAULT_IVP_SD,
+        "E_0": DEFAULT_IVP_SD,
+        "I_0": DEFAULT_IVP_SD,
+        "R_0": DEFAULT_IVP_SD,
+    },
+    init_names=["S_0", "E_0", "I_0", "R_0"],
+)
+COOLING_RATE = 0.5
+
+measles_box = {
+    "R0": [10.0, 60.0],
+    "sigma": [25.0, 100.0],
+    "gamma": [25.0, 320.0],
+    "iota": [0.004, 3.0],
+    "rho": [0.1, 0.9],
+    "sigmaSE": [0.04, 0.1],
+    "psi": [0.05, 3.0],
+    "cohort": [0.1, 0.7],
+    "amplitude": [0.1, 0.6],
+    "S_0": [0.01, 0.07],
+    "E_0": [0.000004, 0.0001],
+    "I_0": [0.000003, 0.001],
+    "R_0": [0.9, 0.99],
+}
+
+key, subkey = jax.random.split(key)
+
+dummy_initial_params_list = pp.Pomp.sample_params(measles_box, NREPS_FITR, key=subkey)
+
+initial_params = pp.PanelPomp.sample_params(
+    measles_box,
+    n=NREPS_FITR,
+    units=UNITS,
+    key=subkey,
+    shared_names=[],
+)
+
+pomp_dict = {
+    unit: pp.UKMeasles.Pomp(
+        unit=[unit],
+        theta=dummy_initial_params_list,
+        model="003",
+        clean=True,
+    )
+    for unit in UNITS
+}
+
+panel_measles_obj = pp.PanelPomp(
+    Pomp_dict=pomp_dict,
+    theta=pp.PanelParameters(theta=initial_params),
+)
